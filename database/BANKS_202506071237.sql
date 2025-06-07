c4256566526bd0199107b9d7405e36be62c20047
Eyoel Tekle <eyoeltekle10@gmail.com>
2025-06-07 12:44:26 +0300

    feat: task 3 completion

---
+++ database/BANKS_202506071237.sql
@@ -0,0 +1,4 @@
+INSERT INTO BANK_REVIEWS.BANKS (BANK_ID,BANK_NAME,APP_ID) VALUES
+	 (1,'Commercial Bank of Ethiopia','com.combanketh.mobilebanking'),
+	 (2,'Bank of Abysinnia','com.boa.boaMobileBanking'),
+	 (3,'Dashen Bank','com.dashen.dashensuperapp');
